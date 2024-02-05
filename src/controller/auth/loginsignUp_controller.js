53ffd9c2c72a6c58ec44c8e3b2e3d4c2a05e5dff
abhishekcad001 <abhishekcad001@gmail.com>
2024-02-05 22:24:17 +0530

    feat : change password

--- src/controller/auth/loginsignUp_controller.js
+++ src/controller/auth/loginsignUp_controller.js
@@ -2,8 +2,14 @@ const { createToken } = require("../../authentication/jwt_token");
 const { addUserValidation } = require("../../config/joi.validation");
 const UserModel = require("../../model/user_model");
 const ApiError = require("../../utils/error");
-const { compareHash } = require("../../utils/hash");
-const {USER_ROLE}=require("../../config/string")
+const { compareHash, hashPassword } = require("../../utils/hash");
+const { USER_ROLE } = require("../../config/string");
+const generateOtp = require("../../utils/genarate_otp");
+const transporter = require("../../utils/transporter");
+const fs = require("fs");
+const path = require("path");
+const OtpModel = require("../../model/otp_model");
+
 async function createUser(req, res, next) {
     try {
         const id = req.id;
@@ -33,8 +39,8 @@ async function login(req, res, next) {
         if (findUser) {
             const comparePass = compareHash(password, findUser.password);
             if (comparePass == true) {
-                const token = createToken({ id: findUser._id, role: USER_ROLE});
-                res.status(200).json({ success: true, message: "login successfully" ,token: token});
+                const token = createToken({ id: findUser._id, role: USER_ROLE });
+                res.status(200).json({ success: true, message: "login successfully", token: token });
             } else {
                 return next(new ApiError(401, "Password is wrong"));
             }
@@ -45,4 +51,57 @@ async function login(req, res, next) {
         next(new ApiError(400, error.message));
     }
 }
-module.exports = { createUser, login };
+
+async function forgetPasswordVerifyEmail(req, res, next) {
+    try {
+        debugger;
+        const { email } = req.body;
+        const filePath = path.join(__dirname, "../../../public/otp.html");
+        let htmlData = fs.readFileSync(filePath, "utf-8");
+        const otp = generateOtp();
+        htmlData = htmlData.replace("${otp}", otp);
+        transporter.sendMail(
+            {
+                to: email,
+                subject: "Verify email",
+                html: htmlData,
+            },
+            async (err, _result) => {
+                if (err) {
+                    return next(new ApiError(400, err.message));
+                }
+                await OtpModel.deleteMany({ email: email });
+                const otpModel = new OtpModel({ email, otp });
+                await otpModel.save();
+                setTimeout(async () => {
+                    await OtpModel.findByIdAndDelete(otpModel._id);
+                }, 1000 * 60);
+                res.status(200).json({ statusCode: 200, success: true, message: "Otp send your email" });
+            },
+        );
+    } catch (e) {
+        next(new ApiError(400, e.message));
+    }
+}
+
+async function verifyOtpForgetPwd(req, res, next) {
+    try {
+        const { email, otp, newPassword } = req.body;
+        const findOtp = await OtpModel.findOne({ email });
+        if (!findOtp) {
+            return next(new ApiError(400, "Otp Expired"));
+        } else {
+            const hashPwd = hashPassword(newPassword);
+            const newPwdSet = await UserModel.updateOne({ email: email }, { $set: { password: hashPwd } });
+        }
+        if (findOtp.otp !== otp) {
+            return next(new ApiError(400, "Otp is wrong"));
+        }
+        await OtpModel.deleteMany({ email: email });
+        res.status(200).json({ statusCode: 200, success: true, message: "Password change successfully" });
+    } catch (e) {
+        next(new ApiError(400, e.message));
+    }
+}
+
+module.exports = { createUser, login, forgetPasswordVerifyEmail, verifyOtpForgetPwd };
--- src/router/auth/loginsignUp_router.js
+++ src/router/auth/loginsignUp_router.js
@@ -1,9 +1,11 @@
-const {Router}=require("express");
+const { Router } = require("express");
 const { verifyUser } = require("../../middleware/verify_user");
-const { createUser, login } = require("../../controller/auth/loginsignUp_controller");
+const { createUser, login, forgetPasswordVerifyEmail, verifyOtpForgetPwd } = require("../../controller/auth/loginsignUp_controller");
 
-const router=Router();
-router.post("/add-User",createUser)
-router.post("/login",login)
+const router = Router();
+router.post("/add-User", createUser);
+router.post("/login", login);
+router.post("/verify-email", forgetPasswordVerifyEmail);
+router.post("/verify-otp-ChnagePwd", verifyOtpForgetPwd);
 
-module.exports=router;
+module.exports = router;
