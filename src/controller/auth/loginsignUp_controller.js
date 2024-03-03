de98c80891f83d52454c3cd752fb5df7faeb3c3e
Jayrpatel2 <jayrpatel211101@gmail.com>
2024-03-02 23:08:44 -0500

    const findEmail = await UserModel.findOne({ email });

--- src/controller/auth/loginsignUp_controller.js
+++ src/controller/auth/loginsignUp_controller.js
@@ -11,106 +11,114 @@ const path = require("path");
 const OtpModel = require("../../model/otp_model");
 
 async function createUser(req, res, next) {
-    try {
-        const id = req.id;
-        const findUser = await UserModel.findOne({ email: req.body.email });
-        if (findUser) {
-            return next(new ApiError(400, "Already contain this email"));
-        }
-        // req.body.company = id;
-
-        const userValid = addUserValidation.validate(req.body);
-        if (userValid.error) {
-            return next(new ApiError(403, userValid.error.details[0].message));
-        }
-
-        const user = new UserModel(req.body);
-        await user.save();
-        res.status(201).json({ success: true, message: "User added successfully" });
-    } catch (error) {
-        next(new ApiError(400, error.message));
+  try {
+    const id = req.id;
+    const findUser = await UserModel.findOne({ email: req.body.email });
+    if (findUser) {
+      return next(new ApiError(400, "Already contain this email"));
     }
+    // req.body.company = id;
+
+    const userValid = addUserValidation.validate(req.body);
+    if (userValid.error) {
+      return next(new ApiError(403, userValid.error.details[0].message));
+    }
+
+    const user = new UserModel(req.body);
+    await user.save();
+    res.status(201).json({ success: true, message: "User added successfully" });
+  } catch (error) {
+    next(new ApiError(400, error.message));
+  }
 }
 
 async function login(req, res, next) {
-    try {
-        const { email, password } = req.body;
-        const findUser = await UserModel.findOne({ email });
-        if (findUser) {
-            const comparePass = compareHash(password, findUser.password);
-            if (comparePass == true) {
-                const token = createToken({ id: findUser._id, role: USER_ROLE });
-                res.status(200).json({ success: true, message: "login successfully", token: token });
-            } else {
-                return next(new ApiError(401, "Password is wrong"));
-            }
-        } else {
-            return next(new ApiError(401, "Email not exist"));
-        }
-    } catch (error) {
-        next(new ApiError(400, error.message));
+  try {
+    const { email, password } = req.body;
+    const findUser = await UserModel.findOne({ email });
+    if (findUser) {
+      const comparePass = compareHash(password, findUser.password);
+      if (comparePass == true) {
+        const token = createToken({ id: findUser._id, role: USER_ROLE });
+        res.status(200).json({ success: true, message: "login successfully", token: token });
+      } else {
+        return next(new ApiError(401, "Password is wrong"));
+      }
+    } else {
+      return next(new ApiError(401, "Email not exist"));
     }
+  } catch (error) {
+    next(new ApiError(400, error.message));
+  }
 }
 
 async function forgetPasswordVerifyEmail(req, res, next) {
-    try {
-        debugger;
-        const { email } = req.body;
-        const filePath = path.join(__dirname, "../../../public/otp.html");
-        let htmlData = fs.readFileSync(filePath, "utf-8");
-        const otp = generateOtp();
-        htmlData = htmlData.replace("${otp}", otp);
-        transporter.sendMail(
-            {
-                to: email,
-                subject: "Verify email",
-                html: htmlData,
-            },
-            async (err, _result) => {
-                if (err) {
-                    return next(new ApiError(400, err.message));
-                }
-                await OtpModel.deleteMany({ email: email });
-                const otpModel = new OtpModel({ email, otp });
-                await otpModel.save();
-                setTimeout(async () => {
-                    await OtpModel.findByIdAndDelete(otpModel._id);
-                }, 1000 * 60);
-                res.status(200).json({ statusCode: 200, success: true, message: "Otp send your email" });
-            },
-        );
-    } catch (error) {
-        next(new ApiError(400, error.message));
-    }
+  try {
+    debugger;
+    const { email } = req.body;
+    const findEmail = await UserModel.findOne({ email });
+if (findEmail) {
+    const filePath = path.join(__dirname, "../../../public/otp.html");
+    let htmlData = fs.readFileSync(filePath, "utf-8");
+    const otp = generateOtp();
+    htmlData = htmlData.replace("${otp}", otp);
+    transporter.sendMail(
+      {
+        to: email,
+        subject: "Verify email",
+        html: htmlData,
+      },
+      async (err, _result) => {
+        if (err) {
+          return next(new ApiError(400, err.message));
+        }
+        await OtpModel.deleteMany({ email: email });
+        const otpModel = new OtpModel({ email, otp });
+        await otpModel.save();
+        setTimeout(async () => {
+          await OtpModel.findByIdAndDelete(otpModel._id);
+        }, 1000 * 60);
+        res.status(200).json({ statusCode: 200, success: true, message: "Otp send your email" });
+      }
+    );
+} else {
+    res.status(400).json({ statusCode: 400, success: false, message: "This email not exist in the system." });
+    
+}
+
+  
+  } catch (error) {
+    next(new ApiError(400, error.message));
+  }
 }
 
 async function verifyOtpForgetPwd(req, res, next) {
-    try {
-        const { otp } = req.body;
-        const findOtp = await OtpModel.findOne({ otp });
-        if (!findOtp) {
-            return next(new ApiError(400, "Otp Expired"));
-        } else {
-            res.status(200).json({ statusCode: 200, success: true, message: "Otp verify successfully" });
-        }
-    } catch (error) {
-        next(new ApiError(400, error.message))
+  try {
+    const { otp } = req.body;
+    const findOtp = await OtpModel.findOne({ otp });
+    if (!findOtp) {
+      return next(new ApiError(400, "Otp Expired"));
+    } else {
+      res.status(200).json({ statusCode: 200, success: true, message: "Otp verify successfully" });
     }
+  } catch (error) {
+    next(new ApiError(400, error.message));
+  }
 }
 
 async function newPasswordForgetPwd(req, res, next) {
-    try {
-        const { email, newPassword } = req.body;
-        const findEmail = await UserModel.findOne({ email });
-        if (!findEmail) {
-            return next(new ApiError(400, "Email not exist"));
-        } else {
-            const hashPwd = hashPassword(newPassword);
-            const newPwdSet = await UserModel.updateOne({ email: email }, { $set: { password: hashPwd } });
-            res.status(200).json({ statusCode: 200, success: true, message: "Password update successfully" });
-        }
-    } catch (error) {
-        next(new ApiError(400, error.message));
+  try {
+    const { email, newPassword } = req.body;
+    const findEmail = await UserModel.findOne({ email });
+    if (!findEmail) {
+      return next(new ApiError(400, "Email not exist"));
+    } else {
+      const hashPwd = hashPassword(newPassword);
+      const newPwdSet = await UserModel.updateOne({ email: email }, { $set: { password: hashPwd } });
+      res.status(200).json({ statusCode: 200, success: true, message: "Password update successfully" });
     }
+  } catch (error) {
+    next(new ApiError(400, error.message));
+  }
 }
 module.exports = { createUser, login, forgetPasswordVerifyEmail, verifyOtpForgetPwd, newPasswordForgetPwd };
