6bb0a66ab1e58e94d41dd083c5cf4afe1685e803
abhishekcad001 <abhishekcad001@gmail.com>
2024-03-13 22:46:12 +0530

    set lister model

--- src/model/lister_model.js
+++ src/model/lister_model.js
@@ -1,9 +1,64 @@
-const {Schema,model}=require("mongoose")
+const { Schema, model } = require("mongoose");
 
-const listerSchema = new Schema({
+const listerSchema = new Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
 
-})
+        lastName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        contact_Number: {
+            type: Number,
+            required: true,
+        },
+        driving_licence: {
+            type: Number,
+            required: true,
+        },
+        sin_Number: {
+            type: Number,
+            required: true,
+        },
+        home_Address: {
+            type: String,
+            required: true,
+        },
+        bathrooms: {
+            type: Number,
+            required: true,
+        },
+        badrooms: {
+            type: Number,
+            required: true,
+        },
+        link: {
+         type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+        toJSON: {
+            transform: (_doc, ret, _option) => {
+                delete ret._id;
+                delete ret.password;
+            },
+            virtuals: true,
+            versionKey: false,
+        },
+    },
+);
 
-const ListerModel=new model("lister",listerSchema)
+const ListerModel = new model("lister", listerSchema);
 
-module.exports=ListerModel
+module.exports = ListerModel;
+
+
