dc62f44c1855d0ebd86d82281df10ff8f0a58d74
abhishekcad001 <abhishekcad001@gmail.com>
2024-03-14 22:17:35 +0530

    set validation

--- src/config/joi.validation.js
+++ src/config/joi.validation.js
@@ -9,40 +9,53 @@ const addUserValidation = Joi.object().keys({
     password: JoiPassword.string().min(6).minOfSpecialCharacters(1).minOfLowercase(1).minOfUppercase(1).minOfNumeric(1).noWhiteSpaces().required(),
 });
 
-const addHomeValidation=Joi.object().keys({
-    title:Joi.string().required(),
-    description:Joi.string().required(),
-    price:Joi.number().integer().required(),
+const addHomeValidation = Joi.object().keys({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    price: Joi.number().integer().required(),
     // price_After_Tax:,
-    photos:Joi.string(),
-    address:Joi.any().required(),
-   //  address:{
-   //    type:{
-   //       house_no:Joi.number().required(),
-   //       strret_name:Joi.string().required(),
-   //    },},
-    country:Joi.string().required(),
-    state:Joi.string().required(),
-    city:Joi.string().required(),
-    zip:Joi.number().integer().required(),
-    size:Joi.number().integer().required(),
-    rooms:Joi.number().integer().required(),
-    badrooms:Joi.number().integer().required(),
-    bathrooms:Joi.number().integer().required(),
-    garages:Joi.boolean(),
-    garage_size:Joi.string().required(),
-    basement:Joi.string().required(),
-    roofing:Joi.string().required(),
-    floor_no:Joi.number().integer().required(),
-    available_from:Joi.date().required(),
-    interior_details:Joi.string().required(),
-    outdoor_details:Joi.string().required(),
-    utilities:Joi.string().required(),
-    other_features:Joi.string().required(),
-    publicId:Joi.string(),
-})
+    photos: Joi.string(),
+    address: Joi.any().required(),
+    //  address:{
+    //    type:{
+    //       house_no:Joi.number().required(),
+    //       strret_name:Joi.string().required(),
+    //    },},
+    country: Joi.string().required(),
+    state: Joi.string().required(),
+    city: Joi.string().required(),
+    zip: Joi.number().integer().required(),
+    size: Joi.number().integer().required(),
+    rooms: Joi.number().integer().required(),
+    badrooms: Joi.number().integer().required(),
+    bathrooms: Joi.number().integer().required(),
+    garages: Joi.boolean(),
+    garage_size: Joi.string().required(),
+    basement: Joi.string().required(),
+    roofing: Joi.string().required(),
+    floor_no: Joi.number().integer().required(),
+    available_from: Joi.date().required(),
+    interior_details: Joi.string().required(),
+    outdoor_details: Joi.string().required(),
+    utilities: Joi.string().required(),
+    other_features: Joi.string().required(),
+    publicId: Joi.string(),
+});
 
-module.exports={
-   addUserValidation,
-   addHomeValidation
-}
+const becameListerValidation = Joi.object.keys({
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    email: Joi.string().email().required(),
+    contact_Number: Joi.number().required(),
+    driving_licence: Joi.number().required(),
+    sin_Number: Joi.number().required(),
+    home_Address: Joi.any().required(),
+    bathrooms: Joi.number().required(),
+    badrooms: Joi.number().required(),
+    link: Joi.string().required(),
+});
+module.exports = {
+    addUserValidation,
+    addHomeValidation,
+    becameListerValidation,
+};
